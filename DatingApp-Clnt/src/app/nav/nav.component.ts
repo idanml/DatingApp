8f82498d354fa43ca5f94c25d2685941e44b7dfd
Idan <idanml@gmail.com>
2020-06-04 17:20:14 +0300

    section 4

---
+++ DatingApp-Clnt/src/app/nav/nav.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css'],
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {}
+
+  login() {
+    this.authService.login(this.model).subscribe(
+      (next) => {
+        console.log('suceessfull');
+      },
+      (error) => {
+        console.log('error');
+      }
+    );
+  }
+
+  loggedIn(){
+    const token = localStorage.getItem('token');
+    return !!token; // just like use if(token)
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('logged out');
+  }
+}
---
+++ DatingApp-Clnt/src/app/register/register.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+})
+export class RegisterComponent implements OnInit {
+  @Output() cancelReg = new EventEmitter();
+  model: any = {};
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {}
+  register() {
+    this.authService.register(this.model).subscribe(
+      () => {
+        console.log('reg seccess');
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  cancel() {
+    this.cancelReg.emit(false);
+    console.log('cancel');
+  }
+}
